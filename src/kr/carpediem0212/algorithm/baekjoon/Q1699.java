98547dc7900eedbf562f05fd997f30fe83841383
k-carpediem0212 <k.carpediem0212@gmail.com>
2018-04-29 16:13:51 +0900

    Q1699 정리 완료

--- src/kr/carpediem0212/algorithm/baekjoon/Q1699.java
+++ src/kr/carpediem0212/algorithm/baekjoon/Q1699.java
@@ -5,11 +5,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// 1 2 3 4 5 6 7 8 9 10 11
-// 1 2 3 1 2 3 4 2 3 4  5
-// 1 2 3 1 2 3 4 2 1 2  3
-public class Main {
-	public Main() {
+/* 
+ *  **********************************************************
+ *
+ *   Written By k-carpediem0212 (k.carpediem0212@gmail.com)
+ *   
+ *  **********************************************************
+ *
+ *	------------------------------------------------
+ *	조건 :
+ *	------------------------------------------------
+ *	
+ *	num을 표현하는데 사용되는 수는 우선
+ *	num의 제곱근 보다 작은 정수이다.
+ *	
+ *	해결 방법은 1부터 num의 제곱근 보다 큰 정수 까지
+ *	경우의 수를 확인하여 최소가 되는 값을 Return 하면 된다.
+ *	
+ *	동전 문제와 동일하게 풀이가 가능하다.		
+ *
+ *	[점화식]
+ *		cache[j] = Math.min(cache[j], cache[j - pow] + 1);
+ *    
+ *  ---------------------------
+ *  공간복잡도 : O(n)
+ *  시간복잡도 : O(n^2)
+ *  ---------------------------
+ */
+public class Q1699 {
+	public Q1699() {
+		
 	}
 
 	public int presentSqrt(int num) {
@@ -39,7 +64,7 @@ public class Main {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int num = Integer.parseInt(st.nextToken());
 
-			Main m = new Main();
+			Q1699 m = new Q1699();
 			System.out.println(m.presentSqrt(num));
 		} catch (IOException e) {
 			e.printStackTrace();
